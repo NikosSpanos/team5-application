3d1a38647a9efd32c054e850f29a6cd2c3da2518
Nikos Spanos <spanos.nikolaos@outlook.com>
2021-06-02 21:32:29 +0300

    modified vm_connection.sh to extract data from terraform output

--- production_environment/vm_connection.sh
+++ production_environment/vm_connection.sh
@@ -6,13 +6,13 @@ set -x
 # chmod 600 $1/$2-private-key-connector
 #TBD=>private_key_secret=$(az keyvault secret show --name "production-secret" --vault-name "dlksf-team5keyvault" --query "value" | tr -d "\042")
 
-file_key="$1/$2-private-key-connector"
-file_ip="$1/$2-public-ip-value"
+#file_key="$1/$2-private-key-connector"
+#file_ip="$1/$2-public-ip-value"
 
-# file_key_v2=$(terraform output output_private_key)
-# file_ip_v2=$(terraform output output_public_ip | tr -d "\042")
+file_key_v2=$(terraform output -json output_private_key | jq -r '.output_private_key.value')
+file_ip_v2=$(terraform output -json output_public_key | jq -r '.output_public_key.value')
 
-ipaddress=$(cat $file_ip)
+#ipaddress=$(cat $file_ip)
 #echo "You requested to connect to vm instance in ip: $file_ip"
 
-ssh -i $file_key codehubTeam5@$ipaddress
+ssh -i $file_key_v2 codehubTeam5@$file_ip_v2

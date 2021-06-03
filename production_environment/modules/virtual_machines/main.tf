253a9fa530472a03327da54338fb3bbf28e1adb9
NikosSpanos <spanos.nikolaos@outlook.com>
2021-06-03 17:43:45 +0300

    debug isntalling ansible v3

--- production_environment/modules/virtual_machines/main.tf
+++ production_environment/modules/virtual_machines/main.tf
@@ -133,9 +133,9 @@ resource "azurerm_virtual_machine" "vm_prod" {
 
     inline = [
       "sudo apt update",
-      "sudo apt install software-properties-common",
+      "sudo apt install -y software-properties-common",
       "sudo add-apt-repository --yes --update ppa:ansible/ansible",
-      "sudo apt install ansible",
+      "sudo apt install -y ansible",
       "sudo ansible --version"
     ]
   }
